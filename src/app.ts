6c64fb0495ec3e12b0815b6c65ba34b0d7430362
Nathan JAUNET <njaunet@datasolution.fr>
2023-12-05 14:56:08 +0100

    Service Generation

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,21 @@
 import { DockerService } from './dockerService';
+import Handlebars from 'handlebars';
+import fs from "fs";
 
-DockerService.create().then((answers) => {
-  console.log(__dirname);
-  console.log(DockerService.renderServices(answers));
-  // console.log(dump(answers));
-  // console.log(JSON.stringify(answers, null, '  '));
+const serviceTemplateFile = fs.readFileSync(`${__dirname}/templates/service.handlebars`);
+const composeTemplateFile =  fs.readFileSync(`${__dirname}/templates/compose.handlebars`);
+Handlebars.registerHelper(`ifEquals`, function (a, b) {
+  return a === b;
 });
+Handlebars.registerPartial('service', Handlebars.compile(serviceTemplateFile.toString()));
+
+DockerService.create().then((serviceAnswers) => {
+  const composeTemplate = Handlebars.compile(composeTemplateFile.toString());
+  const compose = composeTemplate({serviceAnswers});
+  fs.writeFileSync('./docker-compose.yml', compose);
+  // console.log(JSON.stringify(serviceAnswers, null, '  '));
+});
+
+//TODO: Générer la partie network
+//TODO: Générer les fichiers env
+//TODO: Configuration simple/avancée
--- src/dockerService.ts
+++ src/dockerService.ts
@@ -1,9 +1,9 @@
 import inquirer from 'inquirer';
-import Handlebars from 'handlebars';
-import * as fs from "fs";
 
 export type ServiceAnswer = {
   createService: boolean;
+  advancedConfig: boolean;
+  serviceDoc: string;
   serviceName: string;
   imageName: string;
   imageTag: string;
@@ -20,6 +20,7 @@ export type ServiceAnswer = {
   dns: string[];
   securityOpts: string[];
   sysctls: string[];
+  tty: boolean;
   ports: string[];
   useNetworksAliases: boolean;
   networks: string[];
@@ -35,7 +36,6 @@ export type ServiceAnswer = {
 }
 
 export class DockerService {
-  private static SERVICES_TEMPLATE_FILE = `${__dirname}/templates/services.handlebars`
 
   private static questions = [
     {
@@ -45,16 +45,31 @@ export class DockerService {
       default: false,
     },
     {
+      type: 'confirm',
+      name: 'advancedConfig',
+      message: 'Use advanced configuration?',
+      default: false,
+      when: (answers: ServiceAnswer) => answers.createService,
+    },
+    {
+      type: 'input',
+      name: 'serviceDoc',
+      message: 'Enter service documentation:',
+      when: (answers: ServiceAnswer) => answers.createService,
+    },
+    {
       type: 'input',
       name: 'serviceName',
       message: 'Enter service name:',
       when: (answers: ServiceAnswer) => answers.createService,
+      validate: (answer: string) => answer !== '' ? true : "You need to set a service name"
     },
     {
       type: 'input',
       name: 'imageName',
       message: 'Image name:',
       when: (answers: ServiceAnswer) => answers.createService,
+      validate: (answer: string) => answer !== '' ? true : "You need to set an image name (with registry if needed)"
     },
     {
       type: 'input',
@@ -73,89 +88,102 @@ export class DockerService {
     {
       type: 'input',
       name: 'user',
+      message: 'User:',
+      when: (answers: ServiceAnswer) => answers.createService && answers.advancedConfig,
+    },
+    {
+      type: 'input',
+      name: 'entrypoint',
       message: 'Entrypoint:',
-      when: (answers: ServiceAnswer) => answers.createService,
+      when: (answers: ServiceAnswer) => answers.createService && answers.advancedConfig,
     },
     {
       type: 'input',
       name: 'command',
       message: 'Command to execute:',
-      when: (answers: ServiceAnswer) => answers.createService,
+      when: (answers: ServiceAnswer) => answers.createService && answers.advancedConfig,
     },
     {
       type: 'input',
       name: 'dependsOn',
       message: 'Dependencies (comma-separated):',
-      filter: (input: string) => input.split(',').map((item) => item.trim()),
+      filter: (input: string) => input.length === 0 ? [] : input.split(',').map((item) => item.trim()),
       when: (answers: ServiceAnswer) => answers.createService,
     },
     {
       type: 'input',
       name: 'labels',
       message: 'Labels (comma-separated):',
-      filter: (input: string) => input.split(',').map((item) => item.trim()),
+      filter: (input: string) => input.length === 0 ? [] : input.split(',').map((item) => item.trim()),
       when: (answers: ServiceAnswer) => answers.createService,
     },
     {
       type: 'input',
       name: 'envFiles',
       message: 'Environment file(s) (comma-separated):',
-      filter: (input: string) => input.split(',').map((item) => item.trim()),
+      filter: (input: string) => input.length === 0 ? [] : input.split(',').map((item) => item.trim()),
       when: (answers: ServiceAnswer) => answers.createService,
     },
     {
       type: 'input',
       name: 'volumePaths',
       message: 'Volume path(s) (comma-separated):',
-      filter: (input: string) => input.split(',').map((item) => item.trim()),
+      filter: (input: string) => input.length === 0 ? [] : input.split(',').map((item) => item.trim()),
       when: (answers: ServiceAnswer) => answers.createService,
     },
     {
       type: 'input',
       name: 'runtime',
       message: 'Runtime:',
-      when: (answers: ServiceAnswer) => answers.createService,
+      when: (answers: ServiceAnswer) => answers.createService && answers.advancedConfig,
     },
     {
       type: 'input',
       name: 'capabilities',
       message: 'Additional capabilities (comma-separated):',
-      filter: (input: string) => input.split(',').map((item) => item.trim()),
-      when: (answers: ServiceAnswer) => answers.createService,
+      filter: (input: string) => input.length === 0 ? [] : input.split(',').map((item) => item.trim()),
+      when: (answers: ServiceAnswer) => answers.createService && answers.advancedConfig,
     },
     {
       type: 'input',
       name: 'dns',
       message: 'DNS servers (comma-separated):',
-      filter: (input: string) => input.split(',').map((item) => item.trim()),
-      when: (answers: ServiceAnswer) => answers.createService,
+      filter: (input: string) => input.length === 0 ? [] : input.split(',').map((item) => item.trim()),
+      when: (answers: ServiceAnswer) => answers.createService && answers.advancedConfig,
     },
     {
       type: 'input',
       name: 'securityOpts',
       message: 'Security options (comma-separated):',
-      filter: (input: string) => input.split(',').map((item) => item.trim()),
-      when: (answers: ServiceAnswer) => answers.createService,
+      filter: (input: string) => input.length === 0 ? [] : input.split(',').map((item) => item.trim()),
+      when: (answers: ServiceAnswer) => answers.createService && answers.advancedConfig,
     },
     {
       type: 'input',
       name: 'sysctls',
       message: 'Sysctls (comma-separated):',
-      filter: (input: string) => input.split(',').map((item) => item.trim()),
-      when: (answers: ServiceAnswer) => answers.createService,
+      filter: (input: string) => input.length === 0 ? [] : input.split(',').map((item) => item.trim()),
+      when: (answers: ServiceAnswer) => answers.createService && answers.advancedConfig,
+    },
+    {
+      type: 'confirm',
+      name: 'tty',
+      message: 'TTY?',
+      default: false,
+      when: (answers: ServiceAnswer) => answers.createService && answers.advancedConfig,
     },
     {
       type: 'input',
       name: 'ports',
       message: 'Exposed ports (comma-separated):',
-      filter: (input: string) => input.split(',').map((item) => item.trim()),
+      filter: (input: string) => input.length === 0 ? [] : input.split(',').map((item) => item.trim()),
       when: (answers: ServiceAnswer) => answers.createService,
     },
     {
       type: 'input',
       name: 'networks',
       message: 'Networks (comma-separated):',
-      filter: (input: string) => input.split(',').map((item) => item.trim()),
+      filter: (input: string) => input.length === 0 ? [] : input.split(',').map((item) => item.trim()),
       when: (answers: ServiceAnswer) => answers.createService,
     },
     {
@@ -163,14 +191,14 @@ export class DockerService {
       name: 'useNetworksAliases',
       message: 'Use networks aliases:',
       default: false,
-      when: (answers: ServiceAnswer) => answers.createService,
+      when: (answers: ServiceAnswer) => answers.createService && answers.advancedConfig,
     },
     {
       type: 'input',
       name: 'networksAliases',
       message: 'Networks aliases (comma-separated aliases and pipe separated networks):',
-      filter: (input: string) => input.split('|').map((item) => item.split(',').map(alias => alias.trim())),
-      when: (answers: ServiceAnswer) => answers.createService && answers.useNetworksAliases,
+      filter: (input: string) => input.length === 0 ? [] : input.split('|').map((item) => item.split(',').map(alias => alias.trim())),
+      when: (answers: ServiceAnswer) => answers.createService && answers.advancedConfig && answers.useNetworksAliases,
     },
     {
       type: 'confirm',
@@ -181,39 +209,45 @@ export class DockerService {
     },
     {
       type: 'input',
-      name: 'healthcheckTest',
+      name: 'healthcheck',
       message: 'Healthcheck test command (comma-separated):',
-      filter: (input: string) => input.split(',').map((item) => item.trim()),
+      validate: (answer: string[]) =>  answer.length !== 0 ? true : "You need to set a healthcheck test command",
+      filter: (input: string) => input.length === 0 ? [] : input.split(',').map((item) => item.trim()),
       when: (answers: ServiceAnswer) => answers.createService && answers.useHealthcheck,
     },
     {
       type: 'input',
       name: 'healthcheckInterval',
       message: 'Healthcheck interval:',
+      default: '1m30s',
       when: (answers: ServiceAnswer) => answers.createService && answers.useHealthcheck,
     },
     {
       type: 'input',
       name: 'healthcheckTimeout',
       message: 'Healthcheck timeout:',
+      default: '10s',
       when: (answers: ServiceAnswer) => answers.createService && answers.useHealthcheck,
     },
     {
       type: 'number',
       name: 'healthcheckRetries',
       message: 'Healthcheck retries:',
+      default: 3,
       when: (answers: ServiceAnswer) => answers.createService && answers.useHealthcheck,
     },
     {
       type: 'input',
       name: 'healthcheckStartPeriod',
       message: 'Healthcheck start period:',
+      default: '40s',
       when: (answers: ServiceAnswer) => answers.createService && answers.useHealthcheck,
     },
     {
       type: 'input',
       name: 'healthcheckStartInterval',
       message: 'Healthcheck start interval:',
+      default: '5s',
       when: (answers: ServiceAnswer) => answers.createService && answers.useHealthcheck,
     },
     {
@@ -233,16 +267,4 @@ export class DockerService {
     }
     return answers;
   }
-
-  static renderServices(answers: ServiceAnswer[]): string {
-    const file = fs.readFileSync(this.SERVICES_TEMPLATE_FILE);
-    const template = Handlebars.compile(file.toString());
-    answers.forEach((answer) => {
-      if (answer.createService) {
-        console.log(template({answer}));
-      }
-    });
-    return '';
-  }
-
 }
